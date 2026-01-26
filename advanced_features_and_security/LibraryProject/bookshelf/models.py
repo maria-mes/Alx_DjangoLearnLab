5085f48f19dfc31a5801297427f84abd546f75ab
maria-mes <maramessay655@gmail.com>
2026-01-26 20:20:05 +0300

    Task 0: CustomUser in advanced_features_and_security/bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+# --- Custom User Manager ---
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
@@ -16,6 +17,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(username, email, password, **extra_fields)
 
+# --- Custom User ---
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
@@ -23,12 +25,23 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
+    class Meta:
+        permissions = [
+            ("can_view", "Can view user"),
+            ("can_create", "Can create user"),
+            ("can_edit", "Can edit user"),
+            ("can_delete", "Can delete user"),
+        ]
+
     def __str__(self):
         return self.username
-class Meta:
-    permissions = [
-        ("can_view", "Can view user"),
-        ("can_create", "Can create user"),
-        ("can_edit", "Can edit user"),
-        ("can_delete", "Can delete user"),
-    ]
+
+# --- Book Model (needed for checker) ---
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
