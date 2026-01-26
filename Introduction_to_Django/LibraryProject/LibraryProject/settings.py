7a58303ef9dae39126e8b36b984c6fc287870399
maria-mes <maramessay655@gmail.com>
2026-01-26 19:46:58 +0300

    Task 0: CustomUser in bookshelf

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -98,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 
 # Internationalization
