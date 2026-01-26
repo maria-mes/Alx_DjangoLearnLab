20d7d1d257c9e4e3349751a44511586a3af41621
maria-mes <maramessay655@gmail.com>
2026-01-26 21:01:21 +0300

    Task 0: CustomUser in advanced_features_and_security/bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import ExampleForm  # if you have a form defined
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # <-- enforce permissions here
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
