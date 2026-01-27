094d95219ea3708c3f8cd421e6878c1a69bd6d29
maria-mes <maramessay655@gmail.com>
2026-01-27 16:11:02 +0300

    Add relationship_app urls.py and update project urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-from relationship_app import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('authors/', views.list_authors, name='authors'),
     path('books/', views.list_books, name='books'),
     path('libraries/', views.list_libraries, name='libraries'),
     path('librarians/', views.list_librarians, name='librarians'),
+    path('libraries/<int:library_id>/', views.library_detail, name='library_detail'),
 ]
