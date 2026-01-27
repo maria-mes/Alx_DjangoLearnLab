e150321e76545364698a1b41ab91ba0cb977eb42
maria-mes <maramessay655@gmail.com>
2026-01-27 16:54:28 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,4 +32,56 @@ def change_book(request, book_id):
 
 @permission_required('relationship_app.can_delete_book')
 def delete_book(request, book_id):
-    return render(request, 'relationship
+    return render(request, 'relationship_app/delete_book.html')
+
+
+# --- Role-based views ---
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+
+# --- Authentication views ---
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('authors')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# --- Library and author/book views ---
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
+
+def list_authors(request):
+    authors = Author.objects.all()
+    return render(request, 'relationship_app/authors.html', {'authors': authors})
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def list_libraries(request):
+    libraries = Library.objects.all()
+    return render(request, 'relationship_app/libraries.html', {'libraries': libraries})
+
+
+# --- Class-based view for library detail ---
+class LibraryDetailView(View):
+    def get(self, request, library_id):
+        library = get_object_or_404(Library, id=library_id)
+        return render(request, 'relationship_app/library_detail.html', {'library': library})
