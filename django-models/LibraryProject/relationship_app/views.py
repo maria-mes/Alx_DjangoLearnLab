2fee8bc9318b61379421772bf728caeff7e8e381
maria-mes <maramessay655@gmail.com>
2026-01-27 15:56:38 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Author, Book, Library, Librarian
 
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id) # <-- use library object 
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
+    
 def list_authors(request):
     authors = Author.objects.all()
     return render(request, 'relationship_app/authors.html', {'authors': authors})
