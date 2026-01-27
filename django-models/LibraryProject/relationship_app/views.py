1896de1a6f7984e791333f02b087080f7efd4483
maria-mes <maramessay655@gmail.com>
2026-01-27 15:47:43 +0300

    Task 0: Add relationship_app with models and queries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
 
-# Create your views here.
+def list_authors(request):
+    authors = Author.objects.all()
+    return render(request, 'relationship_app/authors.html', {'authors': authors})
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/books.html', {'books': books})
+
+def list_libraries(request):
+    libraries = Library.objects.all()
+    return render(request, 'relationship_app/libraries.html', {'libraries': libraries})
+
+def list_librarians(request):
+    librarians = Librarian.objects.all()
+    return render(request, 'relationship_app/librarians.html', {'librarians': librarians})
