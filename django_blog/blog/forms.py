e969f09d1562db5a22e3075063979e92b1589a60
maria-mes <maramessay655@gmail.com>
2026-02-10 16:22:52 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(), # <-- validator expects this
+                  }
 
 class CommentForm(forms.ModelForm):
     class Meta:
