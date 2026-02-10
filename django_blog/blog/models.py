86398d0954b3485e8456ed342aadf625bee6e57a
maria-mes <maramessay655@gmail.com>
2026-02-10 16:20:31 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,12 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Post
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
