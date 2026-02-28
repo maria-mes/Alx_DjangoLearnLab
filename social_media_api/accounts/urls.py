e5293b783e98d2462a4a1ab6903e49ffb705b49b
maria-mes <maramessay655@gmail.com>
2026-02-28 22:40:09 +0300

    Add user routes with DefaultRouter for CRUD actions

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
-from .views import RegisterView, ProfileView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import RegisterView, ProfileView, UserViewSet
+
+# Router for user actions (CRUD, follow/unfollow)
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
+    # Authentication endpoints
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", obtain_auth_token, name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
-    
+
+    # User routes (including follow/unfollow)
+    path("", include(router.urls)),
 ]
