05972a5774c89564336846aa5fb4c15e810c26ba
maria-mes <maramessay655@gmail.com>
2026-02-28 22:34:12 +0300

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path('posts/', include('posts.urls')),
+    path('api/', include('posts.urls')),
 
 ]
