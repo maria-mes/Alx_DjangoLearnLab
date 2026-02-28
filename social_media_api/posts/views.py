79bee8756af071e1f6bea736205bc818f3945b70
maria-mes <maramessay655@gmail.com>
2026-02-28 22:38:49 +0300

    Implement feed view for user posts
    Add feed view to retrieve posts from followed users

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.response import Response
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -19,3 +20,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def feed(request):
+    followed_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
