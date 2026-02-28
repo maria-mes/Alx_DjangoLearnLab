668830da0ef068718e4d28239a7b8490cd539ced
maria-mes <maramessay655@gmail.com>
2026-02-28 23:13:17 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,6 +4,13 @@ from rest_framework.response import Response
 from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from notifications.models import Notification  # import Notification model
+from .models import Tag
+from .serializers import TagSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 # CRUD for posts
 class PostViewSet(viewsets.ModelViewSet):
