640d8201b601eb89349f5986ca0bcb904818a3ee
maria-mes <maramessay655@gmail.com>
2026-02-28 22:43:47 +0300

    Refactor user follow/unfollow functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,43 +1,36 @@
-from rest_framework import generics, permissions, viewsets, status
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from django.contrib.auth import get_user_model
 from .models import CustomUser
-from .serializers import RegisterSerializer, ProfileSerializer, UserSerializer
+from .serializers import UserSerializer
 
 User = get_user_model()
 
-# Register new users
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
-    permission_classes = [permissions.AllowAny]
-
-
-# View or update your own profile
-class ProfileView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
-
-
-# User actions: list, detail, follow/unfollow
-class UserViewSet(viewsets.ModelViewSet):
+# Follow a user
+class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=True, methods=['post'])
-    def follow(self, request, pk=None):
-        user_to_follow = self.get_object()
-        request.user.following.add(user_to_follow)
-        return Response({'status': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+    def post(self, request, pk):
+        try:
+            user_to_follow = CustomUser.objects.get(pk=pk)
+            request.user.following.add(user_to_follow)
+            return Response({'status': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    @action(detail=True, methods=['post'])
-    def unfollow(self, request, pk=None):
-        user_to_unfollow = self.get_object()
-        request.user.following.remove(user_to_unfollow)
-        return Response({'status': f'You unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
+
+# Unfollow a user
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        try:
+            user_to_unfollow = CustomUser.objects.get(pk=pk)
+            request.user.following.remove(user_to_unfollow)
+            return Response({'status': f'You unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
