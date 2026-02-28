833a8f00a53f55df8ad3ac8f96275f5ea45b547d
maria-mes <maramessay655@gmail.com>
2026-02-28 22:47:43 +0300

    Add Like model to handle post likes

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.conf import settings
+from .models import Post
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')  # prevent duplicate likes
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
