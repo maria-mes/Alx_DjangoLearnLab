c155529a17511c9d56f5090fa48a470e805c754e
maria-mes <maramessay655@gmail.com>
2026-02-28 22:36:05 +0300

    Add bio and profile picture fields to CustomUser

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     following = models.ManyToManyField(
         'self',
         symmetrical=False,
